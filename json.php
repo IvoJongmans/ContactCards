a69b77c2da15a1dc7ab19e76a90790903c44e289
Ivo Jongmans <ivojongmans@gmail.com>
2019-05-17 00:13:12 +0200

    create a json.php file that outputs the DB entries in JSON format to the screen

---
+++ json.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JSON</title>
+</head>
+<body>
+
+<?php
+
+require_once "login.php";
+
+try {
+    $conn = new PDO("mysql:host=$hn;dbname=$db", $un, $pw);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br/>"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+$sql = "SELECT * FROM names";
+
+$run = $conn->prepare($sql);
+$run->execute();
+
+$fetch = array();
+
+while($row = $run->fetch(PDO::FETCH_ASSOC)) {
+    $fetch['names'][] = $row;
+}
+
+echo json_encode($fetch);
+
+
+?>
+    
+</body>
+</html>
